2c3bd09b79b99d9266e0710f786fd4dc197b6b6d
estifanosyohannes <estifanosayele75@gmail.com>
2024-07-19 20:20:50 +0300

    final

--- netflix-clone/src/components/Header/Header.js
+++ netflix-clone/src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import './header.css'
+import "./header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
 import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
+import Netflixlogo from "../../assets/images/netflix-logo-history-32.png";
 function Header() {
   return (
     <div className="header_outer_container">
@@ -11,9 +12,8 @@ function Header() {
         <div className="header_left">
           <ul>
             <li>
-              {/* <img src={Netflixlogo} alt="Netflix logo" width="100" /> */}
+              <img src={Netflixlogo} alt="Netflix logo" width="100" />
             </li>
-            <li>Netflixlogo</li>
             <li>Home</li>
             <li>TVShows </li>
             <li>Movies</li>
